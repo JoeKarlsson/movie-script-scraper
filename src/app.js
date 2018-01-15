73a77e8dba35b08f0de64a619ce274ecce5c2966
Joe Karlsson <josephcarlson1@gmail.com>
2018-01-15 16:18:12 -0600

    refactored.

---
+++ src/app.js
@@ -0,0 +1,63 @@
+/*
+	eslint-disable prefer-destructuring
+*/
+
+const _ = require('lodash');
+const getURLs = require('./getURLs');
+const minimist = require('minimist');
+
+const mss = argv => {
+	// List of valid genres
+	const genres = [
+		'Action',
+		'Adventure',
+		'Animation',
+		'Comedy',
+		'Crime',
+		'Drama',
+		'Family',
+		'Fantasy',
+		'Film-Noir',
+		'Horror',
+		'Musical',
+		'Mystery',
+		'Romance',
+		'Sci-Fi',
+		'Short',
+		'Thriller',
+		'War',
+		'Western',
+	];
+
+	// Set default genre and total number of scripts
+	let genre = 'Action';
+	let total = 0;
+
+	// Check if it's a valid genre and if a total number of scripts was given
+	if (!argv.genre && !argv.total) {
+		console.log(`Getting all the scripts for movies of ${genre}`);
+	} else if (!argv.genre && argv.total) {
+		total = argv.total;
+		console.log(`Getting ${total} random scripts for movies of ${genre}`);
+	} else if (!_.contains(genres, argv.genre)) {
+		console.log('Sorry, invalid genre.');
+		return;
+	} else {
+		genre = argv.genre;
+		if (argv.total) {
+			total = argv.total;
+			console.log(`Getting ${total} random scripts for movies of ${genre}`);
+		} else {
+			console.log(`Getting all scripts for movies of ${genre}`);
+		}
+	}
+
+	getURLs(genre, total);
+};
+
+// Process command line arguments
+const argv = minimist(process.argv.slice(2));
+
+mss(argv);
+
+module.export = mss;
---
+++ src/checkDirectory.js
@@ -0,0 +1,24 @@
+const util = require('util');
+const fs = require('fs');
+
+const stat = util.promisify(fs.stat);
+const mkdirp = require('mkdirp');
+
+const checkDirectory = (genre, callback) => {
+	console.log('genre', genre);
+	// Create directory if it doesn't exist
+	stat(`scripts/${genre}/`)
+		.then(() => {
+			// Execute callback
+			callback();
+		})
+		.catch(() => {
+			mkdirp(`scripts/${genre}/`, err => {
+				if (err) return console.log(`Failed to make directory for ${genre}`);
+				// Execute callback
+				callback();
+			});
+		});
+};
+
+module.exports = checkDirectory;
