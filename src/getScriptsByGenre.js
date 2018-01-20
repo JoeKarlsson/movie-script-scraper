af8ca00632d8d7e755e266be3cc264b3652d5925
Joe Karlsson <josephcarlson1@gmail.com>
2018-01-19 19:37:06 -0600

    dest working

--- src/getScriptsByGenre.js
+++ src/getScriptsByGenre.js
@@ -16,25 +16,6 @@ const removeInvalidURLs = urls => {
 	});
 };
 
-// const randomlyRetreiveScript = async (url, genre, total) => {
-// 	let totalCounter = 0;
-//
-// 	// Randomly choosing if script shall be saved
-// 	if (total !== 0) {
-// 		if (totalCounter === total) return;
-// 		const totalRandomNumber = randomNum();
-// 		if (totalRandomNumber % 3 !== 0) return; // Don't save
-// 	}
-//
-// 	const filePath = await getScript(url, genre, total);
-// 	console.log('filePath', filePath);
-//
-// 	// Increment total counter
-// 	if (total !== 0) ++totalCounter;
-//
-// 	return filePath;
-// };
-
 const addScriptsToDir = async (urls, genre, total, dest) => {
 	let totalCounter = 0;
 
